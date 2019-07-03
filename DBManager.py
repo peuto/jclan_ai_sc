155ec46c69761c67d73d29241d2714f6df260ce0
Augear <augear@foxmail.com>
2019-07-03 15:07:10 +0800

    Add DB Model

---
+++ DBManager.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+
+class DBManager:
+
+    # engine = create_engine('mysql+mysqlconnector://root:123456@localhost:3306/test')
+    def __init__(self, db, host, usr, pwd, db_name):
+        self.engine = create_engine(db+'://'+usr+':'+pwd+'@'+host+'/'+db_name)
--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from flask import Flask, request
 from flask_cors import *
+from DBManager import *
+
 import json
 
 app = Flask(__name__)
@@ -8,6 +10,11 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True)
 # 解决中文显示的问题
 app.config['JSON_AS_ASCII'] = False
+
+
+db_mananager = DBManager("mysql+mysqlconnector","root","jclan@2019","mysql57.rdsmlq1es7zb7bt.rds.gz.baidubce.com", "JClan_AI")
+if db_mananager != None:
+    print("DB is OK!")
 
 
 @app.route('/')
